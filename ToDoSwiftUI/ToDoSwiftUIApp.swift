da3935a16ec765ef1d0c30cb7cb7751dffa26228
Ullash <ullashpodder.kaz@gmail.com>
2023-08-27 11:41:47 +0100

    Initial Commit

---
+++ ToDoSwiftUI/ToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUIApp.swift
+//  ToDoSwiftUI
+//
+//  Created by Ullash Podder on 27/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
