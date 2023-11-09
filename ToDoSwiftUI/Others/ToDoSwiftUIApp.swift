6d0126e7a91ffc879d310f835d58f9b4490d0fc2
Ullash <ullashpodder.kaz@gmail.com>
2023-11-09 16:30:34 +0000

    First commit. added swift packages needed for firebase

--- ToDoSwiftUI/Others/ToDoSwiftUIApp.swift
+++ ToDoSwiftUI/Others/ToDoSwiftUIApp.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
-
+import FirebaseCore
 @main
 struct ToDoSwiftUIApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
